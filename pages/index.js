7ab2ff35c2bc1e08724d1ec8acc1f795e5df5e94
lutfiandri <lutfiandri232@gmail.com>
2023-12-25 11:01:51 +0700

    feat(cms): add configuration for home page

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,12 @@ import DefaultLayout from 'components/layout/DefaultLayout';
 import Image from 'next/image';
 import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
-const SOCMEDS = [
-  {
-    name: 'Github',
-    icon: FaGithub,
-    url: 'https://github.com/lutfiandri',
-  },
-  {
-    name: 'LinkedIn',
-    icon: FaLinkedinIn,
-    url: 'https://linkedin.com/in/lutfiandriyanto',
-  },
-  {
-    name: 'Instagram',
-    icon: FaInstagram,
-    url: 'https://instagram.com/lutfi_andriyanto',
-  },
-];
-
 export default function Home() {
   return (
     <DefaultLayout>
       <Container>
         <div className="flex min-h-screen-no-header flex-col items-center justify-center md:flex-row md:justify-start">
-          <div className="relative mr-0 h-[200px] w-[200px] overflow-hidden rounded-lg md:mr-8">
+          {/* <div className="relative mr-0 h-[200px] w-[200px] overflow-hidden rounded-lg md:mr-8">
             <Image
               src="/images/lutfi-andriyanto.webp"
               alt="Lutfi Andriyanto"
@@ -35,37 +17,16 @@ export default function Home() {
               objectPosition="center"
               priority
             />
-          </div>
+          </div> */}
           <div className="flex flex-col space-y-1 pt-8 text-center md:mt-0 md:text-left">
-            <div className="text-3xl font-bold ">Hi, it&apos;s me</div>
+            <div className="text-3xl font-bold ">Jelajah</div>
             <h1 className="text-4xl font-bold text-gray-dark dark:text-gray-light">
-              Lutfi Andriyanto
+              Kecamatan Batur, Banjarnegara
             </h1>
-            <h2 className="text-lg font-bold">a Webdev Enthusiast!</h2>
-            <div className="flex justify-center gap-4 pt-6 md:justify-start">
-              <Socmeds socmeds={SOCMEDS} />
-            </div>
+            <h2 className="text-lg font-bold">Batur Bertutur - KKN-PPM UGM</h2>
           </div>
         </div>
       </Container>
     </DefaultLayout>
-  );
-}
-
-function Socmeds({ socmeds = [] }) {
-  return (
-    <>
-      {socmeds.map((socmed) => (
-        <a
-          key={socmed.name}
-          href={socmed.url}
-          target="_blank"
-          rel="noreferrer"
-          title={socmed.name}
-        >
-          <socmed.icon className="h-auto w-6 duration-200 hover:text-blue" />
-        </a>
-      ))}
-    </>
   );
 }
