0e5462a0a1ff09d0e5176d5da0afff3eb81986e2
lutfiandri <lutfiandri232@gmail.com>
2024-01-16 13:43:41 +0700

    feat(seo): next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,30 @@
+const siteUrl = 'https://batur.id';
+
+// add your private routes here
+const exclude = ['/admin*', '/uploads*', '/images*', '/img*', '/maps*'];
+
+// Save crawling budget by not fetching SSG meta files
+const NEXT_SSG_FILES = [
+  '/*.json$',
+  '/*_buildManifest.js$',
+  '/*_middlewareManifest.js$',
+  '/*_ssgManifest.js$',
+  '/*.js$',
+];
+
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl,
+  generateRobotsTxt: true,
+  exclude,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: NEXT_SSG_FILES,
+      },
+    ],
+  },
+};
+
+module.exports = config;
